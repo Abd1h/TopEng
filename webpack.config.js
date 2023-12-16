1ad5a223713ba377438d1b2351ec83826e9ea5b8
Adnan <102221244+losingsan3ty@users.noreply.github.com>
2023-12-16 13:03:57 +0300

    nav semi complete

--- webpack.config.js
+++ webpack.config.js
@@ -48,20 +48,7 @@ module.exports = {
           },
         },
       },
-<<<<<<< HEAD
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource', 
-        generator:{
-        filename:"assets/[name][ext]",
-      }
-      },
-     {
-        test: /\.html$/i,
-        loader: "html-loader",
-      }
-    ],
-=======
+
       
        {
         test: /\.(png|jpe?g|gif|svg)$/i,
@@ -80,7 +67,7 @@ module.exports = {
       },
       
       ,]
->>>>>>> f63fbcb13b3845ae122435c97dfbf093c0947d7d
+
   },
   plugins: [
     // html plugin so its create its own dist folder
