0e087894d857aeed4175aaa6980b8e9d083c02a8
Adnan <102221244+losingsan3ty@users.noreply.github.com>
2024-03-15 13:44:02 +0300

    fixed css map issue

--- backend/src/public/js/bundle.js
+++ backend/src/public/js/bundle.js
@@ -9443,7 +9443,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61098" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53057" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
@@ -9588,4 +9588,3 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["../../../../node_modules/parcel-bundler/src/builtins/hmr-runtime.js","index.js"], null)
-//# sourceMappingURL=/bundle.js.map
