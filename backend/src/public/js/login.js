0e12cc326ff72530ade193c54c53b38f89ed5541
Adnan <102221244+losingsan3ty@users.noreply.github.com>
2024-06-03 01:25:47 +0300

    heroku test

--- backend/src/public/js/login.js
+++ backend/src/public/js/login.js
@@ -6,7 +6,7 @@ export const login = async (email, password) => {
   try {
     const res = await axios({
       method: 'POST',
-      url: 'http://127.0.0.1:3000/api/v1/users/login',
+      url: '/api/v1/users/login',
       // url: '/api/v1/users/login',
       data: {
         email,
@@ -17,7 +17,7 @@ export const login = async (email, password) => {
     if (res.data.status === 'success') {
       showAlert('success', 'Logged in successfully!');
       window.setTimeout(() => {
-        window.location.href = 'http://127.0.0.1:3000/me';
+        window.location.href = '/me';
       }, 1500);
     }
   } catch (err) {
@@ -28,8 +28,8 @@ export const signUp = async (email, password, passwordConfirm, name) => {
   try {
     const res = await axios({
       method: 'POST',
-      url: 'http://127.0.0.1:3000/api/v1/users/signup',
-      // url: '/api/v1/users/signup',
+      // url: 'http://127.0.0.1:3000/api/v1/users/signup',
+      url: '/api/v1/users/signup',
       data: {
         email,
         password,
@@ -41,7 +41,7 @@ export const signUp = async (email, password, passwordConfirm, name) => {
     if (res.data.status === 'success') {
       showAlert('success', 'Signed up successfully!');
       window.setTimeout(() => {
-        window.location.href = 'http://127.0.0.1:3000/me';
+        window.location.href = '/me';
       }, 1500);
     }
   } catch (err) {
@@ -53,13 +53,13 @@ export const logout = async () => {
   try {
     const res = await axios({
       method: 'GET',
-      url: 'http://127.0.0.1:3000/api/v1/users/logout',
+      url: '/api/v1/users/logout',
       // url: '/api/v1/users/logout',
     });
     if ((res.data.status = 'success')) {
       showAlert('success', 'Logging Out');
       window.setTimeout(() => {
-        window.location.href = 'http://127.0.0.1:3000/';
+        window.location.href = '/';
       }, 1500);
       // location.reload(true);
     }
