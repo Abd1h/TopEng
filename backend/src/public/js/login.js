25a28ef163e1f6b405b16173603b85f2dd1c8791
Abd1h <abdlh.lllh@gmail.com>
2024-06-03 03:25:18 +0300

    solution 1

--- backend/src/public/js/login.js
+++ backend/src/public/js/login.js
@@ -6,8 +6,8 @@ export const login = async (email, password) => {
   try {
     const res = await axios({
       method: 'POST',
-      url: 'http://127.0.0.1:3000/api/v1/users/login',
-      // url: '/api/v1/users/login',
+      // url: 'http://127.0.0.1:3000/api/v1/users/login',
+      url: '/api/v1/users/login',
       data: {
         email,
         password,
@@ -28,8 +28,8 @@ export const signUp = async (email, password, passwordConfirm, name) => {
   try {
     const res = await axios({
       method: 'POST',
-      url: 'http://127.0.0.1:3000/api/v1/users/signup',
-      // url: '/api/v1/users/signup',
+      // url: 'http://127.0.0.1:3000/api/v1/users/signup',
+      url: '/api/v1/users/signup',
       data: {
         email,
         password,
@@ -53,8 +53,8 @@ export const logout = async () => {
   try {
     const res = await axios({
       method: 'GET',
-      url: 'http://127.0.0.1:3000/api/v1/users/logout',
-      // url: '/api/v1/users/logout',
+      // url: 'http://127.0.0.1:3000/api/v1/users/logout',
+      url: '/api/v1/users/logout',
     });
     if ((res.data.status = 'success')) {
       showAlert('success', 'Logging Out');
