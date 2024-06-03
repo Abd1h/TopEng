5b39238d22ab45a780d092783e6e0fd6ba213d90
Abd1h <abdlh.lllh@gmail.com>
2024-06-03 03:34:00 +0300

    solution 2

--- backend/src/public/js/search.js
+++ backend/src/public/js/search.js
@@ -7,14 +7,14 @@ export const search = async (selectedTags) => {
   try {
     const res = await axios({
       method: 'POST',
-      url: 'http://127.0.0.1:3000/api/v1/search/getSearchResults',
+      url: 'api/v1/search/getSearchResults',
       data: {
         selectedTags,
       },
     });
     if (res.data.status === 'success' && res.data.data.searchResults.length != 0) {
       // If the POST request is successful, redirect to the result page
-      window.location.href = 'http://127.0.0.1:3000/searchresults';
+      window.location.href = '/searchresults';
     } else {
       throw new Error('no matching results. Please adjust your search.');
     }
